c48439b2860e5921c12183b4b8e970688906a7ff
1901703 <92580787+1901703@users.noreply.github.com>
2022-07-14 23:41:17 +0900

    아이디찾기,비번찾기

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+
 from .forms import RegisterForm
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+
 
 def register(request):
-
     if request.method == 'POST':
         user_form = RegisterForm(request.POST)
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
-            return render(request, 'registration/register_done.html', {'new_user':new_user})
+            return render(request, 'registration/register_done.html', {'new_user': new_user})
     else:
         user_form = RegisterForm()
 
-    return render(request, 'registration/register.html',{'form':user_form})
+    return render(request, 'registration/register.html', {'form': user_form})
+
+
+def ForgotIDview(request):
+    context = {}
+    if request.method == 'POST':
+        email = request.POST.get('email')
+
+        try:
+            user = User.objects.get(email=email)
+            if user is not None:
+                    messages.info(request, str(user.username))
+        except:
+            messages.info(request, "존재하지 않는 회원입니다!!")
+    context = {}
+    return render(request, 'registration/forgot_id.html', context)
--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from shop.views import index
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,9 +32,9 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('coupon/', include('coupon.urls')),
     path('order/', include('order.urls')),
+
+
 ]
 
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
