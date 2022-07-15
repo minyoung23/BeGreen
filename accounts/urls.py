f3d7c492a897c44beea3a0931f1bb816de5c8726
2005912 <pmyamy1@naver.com>
2022-07-16 00:52:50 +0900

    first commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,23 @@
 from django.urls import path
 from django.contrib.auth import views as auth_view
-from .views import register
+from .views import *
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name='accounts'
 
 urlpatterns = [
     path('login/', auth_view.LoginView.as_view(), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('register/', register, name='register'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('forgot_id/', views.ForgotIDview, name='forgot_id'),
+    path('update/', update, name='update'),
+    path('delete/', delete, name='delete'),
+    path('password/', password, name='password'),
+    path('change/', change, name='change'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.contrib import messages
+from django.contrib.auth.models import User
 from .forms import RegisterForm
 
 def register(request):
@@ -14,3 +17,63 @@ def register(request):
         user_form = RegisterForm()
 
     return render(request, 'registration/register.html',{'form':user_form})
+
+def ForgotIDview(request):
+    context = {}
+    if request.method == 'POST':
+        email = request.POST.get('email')
+
+        try:
+            user = User.objects.get(email=email)
+            if user is not None:
+                    messages.info(request, str(user.username))
+        except:
+            messages.info(request, "존재하지 않는 회원입니다!!")
+    context = {}
+    return render(request, 'registration/forgot_id.html', context)
+
+#회원정보 수정
+from .forms import CustomUserChangeForm
+from django.contrib.auth.decorators import login_required
+
+
+def change(request):
+    return render(request, 'registration/update.html')
+
+@login_required
+@require_http_methods(['GET','POST'])
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return render(request, 'registration/update.html')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+
+    return render(request, 'registration/change.html', {'form':form})
+
+@login_required
+def delete(request):
+    if request.method == 'POST':
+        request.user.delete()
+        return render(request, 'registration/login.html')
+    return render(request, 'registration/delete.html')
+
+
+#비밀번호 변경 폼
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+
+@login_required
+def password(request):
+    if request.method == 'POST':
+        password_change_form = PasswordChangeForm(request.user, request.POST)
+        if password_change_form.is_valid():
+            # 추가된 부분
+            user = password_change_form.save()
+            update_session_auth_hash(request, user)
+            return render(request, 'registration/login.html')
+    else:
+        password_change_form = PasswordChangeForm(request.user)
+    return render(request, 'registration/password.html', {'password_change_form': password_change_form})
